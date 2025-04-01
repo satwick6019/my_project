849613ec72d1af0bd652688c54eacf3cbd5c9689
satwick6019 <singhsatwick0@gmail.com>
2025-04-01 17:57:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,4 +110,5 @@ def delete_message():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT in env variables
+    app.run(host="0.0.0.0", port=port)
